7ce7cfb0adcd92558e5ad503941c01b847fe3114
Williamrsh <wrodrigues.santos@outlook.com>
2022-02-15 15:48:50 -0300

    Grid na pratica

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React from 'react';
+import {Grid, Paper} from '@material-ui/core';
 import './Home.css';
-
 
 function Home(){
     return(
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/7kuTKym.jpeg" alt="Imagem Tela Inicial" className="img"/>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={8}>
+                    <Paper style={{height:"100vh", background: "lightgrey"}} />
+                </Grid>
+                 <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+                     <Grid item>
+                     <Paper style={{height:"49vh", background: "orange"}} />
+                        <Paper />
+                     </Grid>
+
+                     <Grid item>
+                        <Paper />
+                        <Paper style={{height:"49vh", background: "green"}} />
+                     </Grid>
+
+                </Grid>
+            </Grid>
         </>
     );
 }
