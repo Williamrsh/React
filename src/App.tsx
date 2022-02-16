034576b81869d3f371fef31cbb94c5824ac4dcef
Williamrsh <wrodrigues.santos@outlook.com>
2022-02-15 23:12:33 -0300

    Atualização

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import Home from './paginas/home/Home';
+import './App.css';
+
+function App() {
+  return (
+     <>
+        <Grid container spacing={3}>
+          <Grid item ></Grid>
+          <Grid item></Grid>
+          <Grid item></Grid>
+          <Grid item></Grid>
+          <Grid item></Grid>
+          <Grid item></Grid>
+          <Grid item></Grid>
+        </Grid>
+     </>
+  );
+}
+
+export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,8 +5,12 @@ import './Home.css';
 function Home(){
     return(
         <>
+<<<<<<< Updated upstream
             <h1 className="titulo">Home</h1>
             <img src="https://i.imgur.com/7kuTKym.jpeg" alt="Imagem Tela Inicial" className="img"/>
+=======
+           
+>>>>>>> Stashed changes
         </>
     );
 }
