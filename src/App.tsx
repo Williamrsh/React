e5a5a9d903d050927aedc7cc25547788859963da
Williamrsh <wrodrigues.santos@outlook.com>
2022-02-21 23:17:22 -0300

    implementação da segurança

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer'
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 
 function App() {
@@ -21,6 +22,10 @@ function App() {
         
           <Route path='/home'>
               <Home />
+          </Route>
+          
+          <Route path='/cadastrousuario'>
+              <CadastroUsuario />
           </Route>
         </div>
       </Switch>
---
+++ src/services/Service.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { url } from 'inspector';
+
+export const api = axios.create({
+    baseURL: 'https://blogpessoal-williamrsh.herokuapp.com'
+})
+
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data)
+    }   
+    export const login = async(url: any, dados: any, setDado: any) => {
+        const resposta = await api.post(url, dados)
+        setDado(resposta.data.token)
+    } 
