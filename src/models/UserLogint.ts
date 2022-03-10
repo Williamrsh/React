3609db23e90f5ac9061a478f9480fc342057f223
Williamrsh <wrodrigues.santos@outlook.com>
2022-03-10 17:51:27 -0300

    Ajuste para o deploy

---
+++ src/models/UserLogint.ts
@@ -0,0 +1,7 @@
+interface UserLogint{
+    id: number;
+    usuario: string;
+    senha: string;
+    token?: string|null;
+}
+export default UserLogint;
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -6,13 +6,13 @@ import './Login.css';
 import { useDispatch } from 'react-redux';
 import { addToken } from '../../store/tokens/actions';
 import { toast } from 'react-toastify';
-import UserLogin from '../../models/UserLogin';
+import UserLogint from '../../models/UserLogint';
 
 function Login() {
     let history = useHistory();
     const dispatch = useDispatch ();
    const [token, setToken] =  useState('');
-    const [userLogin, setUserLogin] = useState<UserLogin>(
+    const [userLogin, setUserLogin] = useState<UserLogint>(
         {
             id: 0,
             usuario: '',
