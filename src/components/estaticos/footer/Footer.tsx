518e0fa89ae04d000b6307a194f8eccd6f50a31a
Williamrsh <wrodrigues.santos@outlook.com>
2022-03-14 20:11:30 -0300

    Atualização da redes sociais

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -22,13 +22,13 @@ function Footer() {
                         <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
                     </Box>
                     <Box display="flex" alignItems="center" justifyContent="center">
-                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                        <a href="https://www.facebook.com/wrodriguesantos/" target="_blank">
                             <FacebookIcon className='redes' />
                         </a>
-                        <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                        <a href="https://www.instagram.com/williamrsh/" target="_blank">
                             <InstagramIcon className='redes' />
                         </a>
-                        <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                        <a href="https://www.linkedin.com/in/williamrsh/" target="_blank">
                             <LinkedInIcon className='redes' />
                         </a>
                     </Box>
